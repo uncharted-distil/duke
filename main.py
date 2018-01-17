58f6a5e68fce1fed8de1c93e7758be682054ff03
David Sullivan <davesullivan41@gmail.com>
2018-01-17 10:35:48 -0600

    Update SentenceProducer class name to conform to pattern of other files, also add a TreeBuilder class and fix some broken filepath issues

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import json
 import time
 import pandas as pd
 
-from ProduceSentence import ProduceSentence
+from SentenceProducer import SentenceProducer
 
 
 def main(model_name='wiki2vec', dataset_name='LL0_1037_ada_prior'):
 
     # EXAMPLE CONFIGURATION
-    ps = ProduceSentence(model_name='/data/duke/models/word2vec/en_1000_no_stem/en.model', 
+    sentenceProducer = SentenceProducer(model_name='/data/duke/models/word2vec/en_1000_no_stem/en.model', 
                         types_filename='/data/duke/models/ontologies/types',
-                        type_heirarchy_filename='type_heirarchy.json',
-                        verbose=True)
+                        type_hierarchy_filename='/data/duke/ontologies/type_hierarchy.json',
+                        inverted_type_hierarchy_filename='/data/duke/ontologies/inverted_type_hierarchy.json',
+                        verbose=False)
 
     csv_path = '/data/duke/data/{0}/{0}_dataset/tables/learningData.csv'.format(dataset_name)
     full_df = pd.read_csv(csv_path, header=0)  # read csv assuming first line has header text. TODO handle files w/o headers
 
-    return ps.produceSentenceFromDataframe(full_df)
+    return sentenceProducer.produceSentenceFromDataframe(full_df)
 
 
 if __name__ == '__main__':

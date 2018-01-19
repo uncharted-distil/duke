009ebe2194acff21726161bb9df91d4c3b458eec
Craig Corcoran <craig@newknowledge.io>
2018-01-18 22:29:18 -0600

    move label generation code to data/ directory, clean up evaluate.py, modify gitignore to be more specific about dataset file types.

--- evaluate.py
+++ evaluate.py
@@ -1,28 +1,19 @@
 import json
-import time
-from datetime import datetime
 
 import numpy as np
-import pandas as pd
-from gensim.models import Word2Vec
-from inflection import pluralize, underscore
 
 from dataset_description import DatasetDescriptor
-from ontologies.ontology import get_tree_file_name
 from similarity_functions import w2v_similarity
-from trees import tree_score
 
-# load dataset labels
-# evaluate performance for score
 
 def evaluate(scores, labels):
     scores = scores if isinstance(scores, np.ndarray) else np.array(scores)
     labels = labels if isinstance(labels, np.ndarray) else np.array(labels)
 
-    assert(labels.dtype == 'int')
-    assert(max(labels) == 1)
-    assert(min(labels) == -1)
-    assert(len(labels) == len(scores))
+    assert labels.dtype == 'int'
+    assert max(labels) == 1
+    assert min(labels) == -1
+    assert len(labels) == len(scores)
 
     results = {}
     pos_inds = np.where(labels == 1)[0]
@@ -30,53 +21,9 @@ def evaluate(scores, labels):
     
     results['positive'] = np.dot(scores[pos_inds], labels[pos_inds]) / len(pos_inds)
     results['negative'] = np.dot(scores[neg_inds], labels[neg_inds]) / len(neg_inds)
-    # results['combined'] = np.dot(scores, labels) / len(labels)
     
     return results
 
-# def evaluate_descriptor(descriptor, dataset_name='185_baseball'):
-
-#     with open('ontologies/{0}'.format(tree_file_name), 'r') as f:  
-#         tree = json.load(f)
-
-
-
-def gen_label_stub(ontology_path='dbpedia_2016-10', prune=False):
-
-    tree_file_name = get_tree_file_name(ontology_path, prune)
-    with open('ontologies/{0}'.format(tree_file_name), 'r') as f:  
-        tree = json.load(f)
-
-    classes = list(tree.keys())
-    df = pd.DataFrame(classes, columns=['class'])
-    df.to_csv('data/class_label_stub.csv', index=False)
-
-
-# def labels_to_related(dataset_name='185_baseball'):
-#     labels_filename = 'data/{0}_labels.csv'.format(dataset_name)
-#     labels = pd.read_csv(labels_filename)
-#     related = list(labels['class'].values)
-
-#     with open('data/{0}_related.csv'.format(dataset_name), 'w') as f:
-#         json.dump(related, f)
-
-# def load_related(dataset_name='185_baseball'):
-#     with open('data/{0}_related.csv'.format(dataset_name), 'r') as f:
-#         return json.load(f)
-
-def fill_labels(dataset_name='185_baseball'):
-
-    labels_filename = 'data/{0}_labels.csv'.format(dataset_name)
-    labels = pd.read_csv(labels_filename)
-    return labels.fillna(-1, downcast='infer')
-    # labels.to_csv(labels_filename, index=False)
-
-def lines_to_json(dataset_name='185_baseball'):
-    with open('data/{0}_positive_examples'.format(dataset_name)) as f:
-        lines = f.readlines()
-    examples = [s.strip() for s in lines]
-    with open('data/{0}_positive_examples.json'.format(dataset_name), 'w') as f:
-        json.dump(examples, f)
 
 def main(
     dataset='185_baseball',
@@ -107,30 +54,12 @@ def main(
     labels_filename = 'data/{0}_positive_examples.json'.format(dataset)
     with open(labels_filename) as f:
         positive_examples = json.load(f)
-
     
     classes = np.array(duke.classes)
     labels = np.array([1 if cl in positive_examples else -1 for cl in classes])    
-    # # labels_filename = 'data/{0}_labels.csv'.format(dataset)
-    # # labels_df = pd.read_csv(labels_filename)
-    # # labels_df.fillna(-1, downcast='infer', inplace=True)  
-
-    # label_map = dict(zip(labels_df['class'], labels_df['label']))
-    # labels = [label_map[cl] for cl in duke.classes]  # remove labels not in duke's classes (out of vocab)
-    # print('num duke classes:', len(classes))
-    # print('num labels:', len(labels))
-    # # assert(all(classes == np.array(duke.classes)))  
-    # pos_inds = np.where(labels == 1)[0]
-    # print('classes related to {0}: {1} \n'.format(dataset, classes[pos_inds]))
 
     print('\n\nevaluation:', evaluate(scores, labels))
 
 
-# plot evaluation for different methods, variants
-
-
 if __name__ == '__main__':
     main()
-    # gen_label_stub()
-    # fill_labels()
-    # lines_to_json()

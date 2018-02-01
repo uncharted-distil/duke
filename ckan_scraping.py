ce4888bdb5883246810813d7a20443f56fd2d877
Craig Corcoran <craig@newknowledge.io>
2018-01-31 23:41:48 -0600

    remove commented out code, add try except block around scraping individual ckan instances

--- ckan_scraping.py
+++ ckan_scraping.py
@@ -125,118 +125,14 @@ def main(data_dir='data/ckan', formats=['xls', 'xlsx', 'csv']):
         instances = json.load(json_file)
     
     for ckan_url in instances:
-        scrape_ckan_instance(ckan_url=ckan_url, formats=formats, data_dir=data_dir)
+        try:
+            scrape_ckan_instance(ckan_url=ckan_url, formats=formats, data_dir=data_dir)
+
+        except Exception as err:
+            print('error scraping ckan instance:', ckan_url)
+            print('error:', err)
     
 
 if __name__ == '__main__':
-    
     # main()
     scrape_ckan_instance(ckan_url="https://catalogue.data.gov.bc.ca")
-
-# def login(usernameV}}, password):
-#     '''
-#     Login to CKAN.
-
-#     Returns a ``requests.Session`` instance with the CKAN
-#     session cookie.
-#     '''
-#     s = requests.Session()
-#     data = {'login': username, 'password': password}
-#     url = CKAN_URL + '/login_generic'
-#     r = s.post(url, data=data)
-# #     if 'field-login' in r.text:
-# #         # Response still contains login form
-# #         raise RuntimeError('Login failed.')
-# #     return s
-# url = 'https://open.alberta.ca'
-
-# instance = RemoteCKAN(url)
-
-# datasets = instance.action.current_package_list_with_resources()
-
-# def download_resource_data(session, pkg_id, res_id):
-#     url = '{ckan}/dataset/{pkg}/resource/{res}/download/'.format(
-#             ckan=CKAN_URL, pkg=pkg_id, res=res_id)
-#     return session.get(url).content
-
-
-    # session = login('craig-corcoran', 'Wh0sRAZ?')
-    # data = download_resource_data(session, 'some-package', 'some-resource')
-    # print(data)
-
-
-
-# from __future__ import print_function, unicode_literals
-# from builtins import range 
-# import sys
-# from ..utils.file_utils import create_dir, process_files
-
-
-# class CkanCrawler(object):
-#     """ Ckan Scraper.
-
-#         CkanCrawler goes through a provided instance/url and downloads all
-#         available datasets.
-
-#     """
-#     def __init__(self, url, interval=50, offset=0, max_datasets=-1, 
-#                  data_dir='data', formats=['XLSX','XLS','CSV'], **kwargs):
-#         self.url                = url
-#         self.interval           = interval 
-#         self.offset             = offset
-#         self.max_datasets       = max_datasets
-#         self.data_dir           = data_dir
-#         self.formats            = formats
-#         self._kwargs            = kwargs
-#         # from imports
-#         self._file_processing   = process_files
-        
-
-#     def get_datasets(self):
-#         instance = RemoteCKAN(self.url)
-#         datasets = instance.action.current_package_list_with_resources(
-#             limit=self.interval, offset=self.offset)
-
-#         instance_dir = create_dir([self.url], self.data_dir)
-
-#         self.num_datasets = 0
-#         for dataset in datasets:
-#             if self.num_datasets <= self.max_datasets:
-#                 for resource in dataset['resources']:
-#                     if resource['format'] in self.formats and resource['url']:
-#                         self.num_datasets += 1
-#                         self._store_dataset(resource['url'], instance_dir)
-#             else:
-#                 print('Hit max number of datasets for {}'.format(self.url))
-#                 return instance_dir
-        
-#         if len(datasets) >= self.interval:
-#             if self.offset <= self.max_datasets:
-#                 self.offset += self.interval
-#                 self.get_datasets()
-#             else:
-#                 print('Hit max number of datasets for {}'.format(self.url))
-#                 return instance_dir
-
-#         # process datasets
-#         self._file_processing(instance_dir, **self._kwargs)
-#         return instance_dir
-
-
-#     def _store_dataset(self, file_url, instance_dir):
-#         local_filename = file_url.split('/')[-1]
-
-#         if not local_filename or local_filename == 'source':
-#             return None
-
-#         r = requests.get(file_url, stream=True)
-#         filename = '/'.join([instance_dir, local_filename])
-#         with open(filename, 'wb') as f:
-#             for chunk in r.iter_content(chunk_size=1024):
-#                 if chunk:
-#                     f.write(chunk)
-        
-    
-# if __name__ == '__main__':
-#    CkanCrawler('', interval=50, offset=0, max_datasets=1000, 
-#                data_dir='data', formats=['XLSX','XLS','CSV'])
